44895b14cfd814ee7a3ecbf7b3afd67c66a63873
丁祖科 <dingzuke@ainnovation.com>
2021-10-13 16:20:08 +0800

    feat: 添加打包lib配置

--- src/index.js
+++ src/index.js
@@ -1,33 +1,22 @@
-import _ from "lodash";
-import './style.css';
-import Icon from './assets/icon.png';
-import printMe from './print.js';
+import _ from 'lodash';
+import numRef from './ref.json';
 
-function component() {
+export function numToWord(num) {
+  return _.reduce(
+    numRef,
+    (accum, ref) => {
+      return ref.num === num ? ref.word : accum;
+    },
+    ''
+  );
+}
 
-    // 显示图片。
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    document.body.appendChild(myIcon);
-
-    // 显示图标 font
-    const divDom = document.createElement('div');
-    var insertText = `<div><h2>图标</h2><span class="iconfont">&#xe600;</span><span class="iconfont">&#xe791;</span></div>`;
-    divDom.innerHTML=(insertText);
-    document.body.appendChild(divDom);
-    
-    // 点击事件
-    const btn = document.createElement('button');
-    btn.innerHTML = '点我看控制台输出1';
-    btn.onclick = printMe;
-    document.body.appendChild(btn);
-
-    // lodash创建一个div class="Hello"
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    return element;
-  }
-  document.body.appendChild(component());
-  
+export function wordToNum(word) {
+  return _.reduce(
+    numRef,
+    (accum, ref) => {
+      return ref.word === word && word.toLowerCase() ? ref.num : accum;
+    },
+    -1
+  );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,26 @@
-
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    // 开发环境使用 代码报错文件（不然报错在bundle文件）
-    devtool: 'inline-source-map',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 9000,
-        hot: true,
-    },
-
-    // 入口
-    entry: {
-        index: './src/index.js',
-        // print: './src/print.js',
-        // module: './src/module.js'
-    },
-    // 输出文件
+    entry: './src/index.js',
     output: {
-        // publicPath 比如部署路径 在 www.baidu.com/web 目录下这里添加/web  一般都是/
-        publicPath: '/',
-        filename: 'js/[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
-        // 资源文件 存放目录
-        assetModuleFilename: 'static/[hash][ext][query]',
         // 清理上次留下的打包
         clean: true,
+        filename: 'webpack-numbers.js',
+        //  umd 配置可以 CommonJS、AMD 以及 script 标签使用
+        library: {
+            name: 'webpackNumbers',
+            type: 'umd',
+          },
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    // 插件
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'webpack-插件管理--'
-        }),
-
-    ],
-    optimization: {
-        moduleIds: 'deterministic',
-        runtimeChunk: 'single',
-        // node_modules 目录的代码打包到 vendor中
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                },
-            },
+    //  library 需要一个名为 lodash 的依赖，这个依赖在 consumer 环境中必须存在且可用。
+    externals: {
+        lodash: {
+          commonjs: 'lodash',
+          commonjs2: 'lodash',
+          amd: 'lodash',
+          root: '_',
         },
-    },
+      },
 };
